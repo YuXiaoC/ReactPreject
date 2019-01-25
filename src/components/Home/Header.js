b4d99457af7348bbd34a43f1e499346ddd1b9b5b
yuyu <1912850912@qq.com>
2019-01-25 11:04:45 +0800

    项目初次提交

---
+++ src/components/Home/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//引入样式
+// import '../CSS/header/index.scss'
+class Header extends React.Component{
+    render(){
+        return <div id="H-search">
+                <p>我是头部</p>
+        </div>
+    } 
+}
+export default Header;
